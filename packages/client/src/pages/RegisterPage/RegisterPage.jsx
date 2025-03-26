bc1a196baa92bf37bd773c83767c5c4319cbe9f8
Ronald Kato <ronaldkato.techlead@gmail.com>
2025-03-26 06:51:01 -0400

    working on implementing a working registration by formik.

--- packages/client/src/pages/RegisterPage/RegisterPage.jsx
+++ packages/client/src/pages/RegisterPage/RegisterPage.jsx
@@ -5,8 +5,8 @@ import "./RegisterPage.scss";
 import { useProvideAuth } from "../../hooks/useAuth";
 import { LandingHeader, LoadingSpinner } from "../../components";
 import { setAuthToken } from "../../utils/api.utils";
-import * as formik from "formik";
 import * as yup from "yup";
+import { useFormik } from "formik";
 
 const initialState = {
   username: "",
@@ -15,6 +15,19 @@ const initialState = {
   isSubmitting: false,
   errorMessage: null,
 };
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .required()
+    .matches(/(?=.*[0-9])/),
+  email: yup.string().email().required(),
+  password: yup
+    .string()
+    .required()
+    .min(10, "Password is too short - should be 8 characters minimum.")
+    .matches(/(?=.*[0-9])/, "Password must contain a number."),
+});
 
 const RegisterPage = () => {
   const [data, setData] = useState(initialState);
@@ -83,99 +96,85 @@ const RegisterPage = () => {
     }
   };
 
-  const { Formik } = formik;
-
-  const validationSchema = yup.object().shape({
-    username: yup
-      .string()
-      .required()
-      .matches(/(?=.*[0-9])/),
-    email: yup.string().email().required(),
-    password: yup
-      .string()
-      .required()
-      .min(10, "Password is too short - should be 8 characters minimum.")
-      .matches(/(?=.*[0-9])/, "Password must contain a number."),
+  const formik = useFormik({
+    initialValues: {
+      username: data.username,
+      email: data.email,
+      password: data.password,
+    },
+    validationSchema,
+    handleChange: handleInputChange(),
+    onSubmit: handleSignup(),
   });
 
   return (
-    <Formik
-      initialValues={{ username: "", email: "", password: "" }}
-      validationSchema={validationSchema}
-    >
-      <div style={{ overflow: "auto", height: "100vh" }}>
-        <LandingHeader />
-        <Container className="mb-5">
-          <Row className="pt-5 justify-content-center">
-            <Form
-              noValidate
-              validated
-              style={{ width: "350px" }}
-              onSubmit={handleSignup}
-            >
-              <h3 className="mb-3">Join Us! </h3>
-              <Form.Group controlId="username-register">
-                <Form.Label>Username</Form.Label>
-                <InputGroup>
-                  <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
-                  <Form.Control
-                    type="text"
-                    name="username"
-                    placeholder="Username"
-                    aria-describedby="inputGroupPrepend"
-                    required
-                    value={data.username}
-                    onChange={handleInputChange}
-                  />
-                </InputGroup>
-              </Form.Group>
-              <Form.Group controlId="email-register">
-                <Form.Label>Email</Form.Label>
+    <div style={{ overflow: "auto", height: "100vh" }}>
+      <LandingHeader />
+      <Container className="mb-5">
+        <Row className="pt-5 justify-content-center">
+          <Form style={{ width: "350px" }} onSubmit={formik.handleSubmit}>
+            <h3 className="mb-3">Join Us! </h3>
+            <Form.Group controlId="username-register">
+              <Form.Label>Username</Form.Label>
+              <InputGroup>
+                <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text>
                 <Form.Control
                   type="text"
-                  name="email"
-                  placeholder="Email"
+                  name="username"
+                  placeholder="Username"
+                  aria-describedby="inputGroupPrepend"
                   required
-                  value={data.email}
-                  onChange={handleInputChange}
+                  value={formik.values.username}
+                  onChange={formik.handleChange}
                 />
-              </Form.Group>
-              <Form.Group>
-                <Form.Label htmlFor="Register">Password</Form.Label>
-                <Form.Control
-                  type="password"
-                  name="password"
-                  required
-                  id="inputPasswordRegister"
-                  value={data.password}
-                  onChange={handleInputChange}
-                />
-              </Form.Group>
-              {data.errorMessage && (
-                <span className="form-error text-warning">
-                  {data.errorMessage}
-                </span>
-              )}
-              <Row className="mr-0">
-                <Col>
-                  Already Registered?
-                  <Button
-                    as="a"
-                    variant="link"
-                    onClick={() => navigate("/login")}
-                  >
-                    Login
-                  </Button>
-                </Col>
-                <Button type="submit" disabled={data.isSubmitting}>
-                  {data.isSubmitting ? <LoadingSpinner /> : "Sign up"}
+              </InputGroup>
+            </Form.Group>
+            <Form.Group controlId="email-register">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                name="email"
+                placeholder="Email"
+                required
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label htmlFor="Register">Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                required
+                id="inputPasswordRegister"
+                value={formik.values.password}
+                onChange={formik.handleChange}
+              />
+            </Form.Group>
+            {data.errorMessage && (
+              <span className="form-error text-warning">
+                {data.errorMessage}
+              </span>
+            )}
+            <Row className="mr-0">
+              <Col>
+                Already Registered?
+                <Button
+                  as="a"
+                  variant="link"
+                  onClick={() => navigate("/login")}
+                >
+                  Login
                 </Button>
-              </Row>
-            </Form>
-          </Row>
-        </Container>
-      </div>
-    </Formik>
+              </Col>
+              <Button type="submit" disabled={data.isSubmitting}>
+                {data.isSubmitting ? <LoadingSpinner /> : "Sign up"}
+              </Button>
+            </Row>
+          </Form>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
